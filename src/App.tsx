c9dba11b321f8b2fd0d3f5255ee1ce5cd2611411
Andy Stocchetti <ajstocchetti@gmail.com>
2024-10-20 15:06:02 -0500

    refresh data when page gets focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useSnapshot } from "valtio";
-import { state } from "src/store/valio";
+import { loadAllData } from "src/data";
 import DropboxUserLogin from "src/pages/login-dropbox";
 import Header from "src/components/header";
 import ErrorBoundary from "src/components/error-boundry";
@@ -10,10 +11,23 @@ import NotFound from "src/pages/not-found";
 import PieceDetails from "src/pages/piece-details";
 import PieceList from "src/pages/piece-list";
 import Tools from "src/pages/tools";
+import { state } from "src/store/valio";
 import "./App.css";
 
 function App() {
   const { isLoggedIn } = useSnapshot(state);
+
+  useEffect(() => {
+    const handleFocus = () => {
+      if (isLoggedIn) loadAllData();
+    };
+    if (isLoggedIn) {
+      window.addEventListener("focus", handleFocus);
+    }
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+    };
+  }, []);
 
   if (!isLoggedIn) return <DropboxUserLogin />;
 
