3ae92a451c4c108458f29a48106226add0d8d5ab
Andy Stocchetti <ajstocchetti@gmail.com>
2026-05-24 09:06:52 -0500

    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,17 @@ export default defineConfig({
       src: "/src",
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // order matters - put react first
+          react: ["react", "react-dom", "react-router-dom"],
+          dropbox: ["dropbox"],
+          antd_icons: ["@ant-design/icons"],
+          antd: ["antd"],
+        },
+      },
+    },
+  },
 });
